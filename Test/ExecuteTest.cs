962fd1a0d937b78cd0134998240754b2580e712a
OTA Daichi <irxnjhtchlnrw@gmail.com>
2010-10-26 06:04:18 +0000

    テストを微修正。
    git-svn-id: https://kurogane.svn.codeplex.com/svn/trunk@78949 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- Test/ExecuteTest.cs
+++ Test/ExecuteTest.cs
@@ -9,6 +9,25 @@ namespace Kurogane.Test {
 	[TestClass]
 	public class ExecuteTest {
 
+		private Engine _engine;
+
+		[TestInitialize]
+		public void Init() {
+			_engine = new Engine();
+			_engine.Execute(
+				"以下の手順でリストを初期値から関数で集約する。" +
+				"	もし(リスト＝無)なら" +
+				"		初期値をパスする。" +
+				"	リストの体を初期値から関数で集約し、次に代入する。" +
+				"	リストの頭と次を関数する。" +
+				"以上。");
+			_engine.Execute(
+				"以下の手順でNをパスする。" +
+				"	Nを返す。" +
+				"以上。");
+		}
+
+
 		[TestMethod]
 		public void 助詞によって引数の順番を切り替える() {
 			string[] codes = {
@@ -18,13 +37,11 @@ namespace Kurogane.Test {
 				"「B」を「C」に「A」がテストする。" ,
 				"「C」に「A」が「B」をテストする。" ,
 				"「C」に「B」を「A」がテストする。"};
-			var engine = new Engine();
-			var scope = engine.Global;
-			scope.SetVariable("テスト", new SuffixFunc<Func<object, object, object, object>>(
+			_engine.Global.SetVariable("テスト", new SuffixFunc<Func<object, object, object, object>>(
 				(a, b, c) => ((string)a == "A" && (string)b == "B" && (string)c == "C"),
 				"が", "を", "に"));
 			foreach (var code in codes) {
-				var result = engine.Execute(code);
+				var result = _engine.Execute(code);
 				Assert.IsInstanceOfType(result, typeof(bool));
 				Assert.IsTrue((bool)result);
 			}
@@ -37,18 +54,13 @@ namespace Kurogane.Test {
 				"1をパスする。" +
 				"※（括弧で囲むよ。複数でもOK！。。。）" +
 				"3をパスする。";
-			var engine = new Engine();
-			engine.Global.SetVariable("パス", SuffixFunc.Create((object obj) => obj, "を"));
-			object num = engine.Execute(code);
+			object num = _engine.Execute(code);
 			Assert.IsInstanceOfType(num, typeof(int));
 			Assert.AreEqual((int)num, 3);
 		}
 
 		[TestMethod]
 		public void フィボナッチ数を計算する() {
-			var engine = new Engine();
-			engine.Global.SetVariable("パス", SuffixFunc.Create((object obj) => obj, "を"));
-
 			string code =
 				"以下の手順でNをFIB変換する。" +
 				"	以下の手順で計算する。" +
@@ -62,7 +74,7 @@ namespace Kurogane.Test {
 				"		計算する。" +
 				"以上。" +
 				"10をFIB変換する。";
-			object num = engine.Execute(code);
+			object num = _engine.Execute(code);
 			Assert.IsInstanceOfType(num, typeof(int));
 			Assert.AreEqual((int)num, 55);
 
@@ -79,7 +91,7 @@ namespace Kurogane.Test {
 				"	0と1をNまでFIB変換する。" +
 				"以上。" +
 				"1000をFIB変換する。";
-			num = engine.Execute(code);
+			num = _engine.Execute(code);
 			Assert.IsInstanceOfType(num, typeof(int));
 			Assert.AreEqual((int)num, 1556111435);
 		}
@@ -90,17 +102,17 @@ namespace Kurogane.Test {
 				"以下の手順でAにBを連結する。" +
 				"	以下の手順でTで判定する。" +
 				"		もし(T＝0)なら" +
-				"			Aをパスする。" +
+				"			Aを返す。" +
 				"		他なら" +
-				"			Bをパスする。" +
+				"			Bを返す。" +
 				"	以上。" +
-				"	判定をパスする。" +
+				"	判定を返す。" +
 				"以上。" +
 				"以下の手順で分解を合計する。" +
 				"	以下の手順で計算する。" +
 				"		0で分解し、Aとする。" +
 				"		1で分解し、合計し、Bとする。" +
-				"		(A+B)をパスする。" +
+				"		(A+B)を返す。" +
 				"	以上。" +
 				"	もし(分解＝0)なら" +
 				"		0をパスする。" +
@@ -109,30 +121,25 @@ namespace Kurogane.Test {
 				"以上。" +
 				"0を1に連結し、2に連結し、3に連結し、4に連結し、リストに代入する。" +
 				"リストを合計する。";
-			var engine = new Engine();
-			engine.Global.SetVariable("パス", SuffixFunc.Create((object obj) => obj, "を"));
-			object num = engine.Execute(code);
+			object num = _engine.Execute(code);
 			Assert.IsInstanceOfType(num, typeof(int));
 			Assert.AreEqual((int)num, 10);
 		}
 
 		[TestMethod]
 		public void 対で引数をとれる() {
-			var code =
+			object num = _engine.Execute(
 				"以下の手順でAとBを加算する。" +
 				"	(A+B)をパスする。" +
 				"以上。" +
-				"3と5を加算する。";
-			var engine = new Engine();
-			engine.Global.SetVariable("パス", SuffixFunc.Create((object obj) => obj, "を"));
-			object num = engine.Execute(code);
+				"3と5を加算する。");
 			Assert.IsInstanceOfType(num, typeof(int));
 			Assert.AreEqual((int)num, 8);
 		}
 
 		[TestMethod]
 		public void 集約関数() {
-			var code =
+			_engine.Execute(
 				"以下の手順でAとBを加算する。" +
 				"	(A＋B)をパスする。" +
 				"以上。" +
@@ -150,23 +157,20 @@ namespace Kurogane.Test {
 				"		計算する。" +
 				"以上。" +
 				"[1, 2, 3, 4, 5]を0から加算で集約し、Aに代入する。" +
-				"[1, 2, 3, 4, 5]を1から乗算で集約し、Bに代入する。";
-			var engine = new Engine();
-			engine.Global.SetVariable("パス", SuffixFunc.Create((object obj) => obj, "を"));
-			engine.Execute(code);
+				"[1, 2, 3, 4, 5]を1から乗算で集約し、Bに代入する。");
 
-			object a = engine.Global.GetVariable("A");
+			object a = _engine.Global.GetVariable("A");
 			Assert.IsInstanceOfType(a, typeof(int));
 			Assert.AreEqual((int)a, 15);
 
-			object b = engine.Global.GetVariable("B");
+			object b = _engine.Global.GetVariable("B");
 			Assert.IsInstanceOfType(b, typeof(int));
 			Assert.AreEqual((int)b, 120);
 		}
 
 		[TestMethod]
 		public void ローカル変数() {
-			string code =
+			_engine.Execute(
 				"「こんにちは」をAに代入する。" +
 				"「さようなら」をBに代入する。" +
 				"以下の手順でテストする。" +
@@ -174,32 +178,27 @@ namespace Kurogane.Test {
 				"	「ごきげんよう」をBとする。" +
 				"	「ようこそ」をCとする。" +
 				"以上。" +
-				"テストする。";
-			var engine = new Engine();
-			engine.Execute(code);
-			var scope = engine.Global;
-			object a = scope.GetVariable("A");
-			object b = scope.GetVariable("B");
-			Assert.AreEqual("おはよう", a);
-			Assert.AreEqual("さようなら", b);
+				"テストする。");
+
+			var scope = _engine.Global;
+			Assert.AreEqual("おはよう", scope.GetVariable("A"));
+			Assert.AreEqual("さようなら", scope.GetVariable("B"));
 			Assert.IsFalse(scope.HasVariable("C"));
 		}
 
 		[TestMethod]
 		public void と_によって対が作られる() {
-			var code =
+			_engine.Execute(
 				"「こんにちは」と「こんばんは」をペアに代入する。" +
 				"ペアの頭をAに代入する。" +
-				"ペアの体をBに代入する。";
-			var engine = new Engine();
-			engine.Execute(code);
-			Assert.AreEqual("こんにちは", engine.Global.GetVariable("A"));
-			Assert.AreEqual("こんばんは", engine.Global.GetVariable("B"));
+				"ペアの体をBに代入する。");
+			Assert.AreEqual("こんにちは", _engine.Global.GetVariable("A"));
+			Assert.AreEqual("こんばんは", _engine.Global.GetVariable("B"));
 		}
 
 		[TestMethod]
 		public void もし文が動く() {
-			string code =
+			_engine.Execute(
 				"3をAに代入する。" +
 				"もし" +
 				"　(A＝無)なら" +
@@ -207,10 +206,9 @@ namespace Kurogane.Test {
 				"　(A＝３)なら" +
 				"　　「正しい」を結果に代入する。" +
 				"　他なら" +
-				"　　　「間違い」を結果に代入する。";
-			var engine = new Engine();
-			engine.Execute(code);
-			Assert.AreEqual("正しい", engine.Global.GetVariable("結果"));
+				"　　　「間違い」を結果に代入する。");
+
+			Assert.AreEqual("正しい", _engine.Global.GetVariable("結果"));
 		}
 	}
 }
