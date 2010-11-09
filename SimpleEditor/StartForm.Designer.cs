5af3d1c03fe9f5ed3e8ee1afa63fc38b1fb10c3e
OTA Daichi <irxnjhtchlnrw@gmail.com>
2010-11-09 05:44:33 +0000

    エディタを作成。
    git-svn-id: https://kurogane.svn.codeplex.com/svn/trunk@79692 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ SimpleEditor/StartForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Kurogane.SimpleEditor {
+	partial class StartForm {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.txtProgram = new System.Windows.Forms.TextBox();
+			this.txtOut = new System.Windows.Forms.TextBox();
+			this.lblProgram = new System.Windows.Forms.Label();
+			this.lblOut = new System.Windows.Forms.Label();
+			this.btnExecute = new System.Windows.Forms.Button();
+			this.splitContainer = new System.Windows.Forms.SplitContainer();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnOpen = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+			this.splitContainer.Panel1.SuspendLayout();
+			this.splitContainer.Panel2.SuspendLayout();
+			this.splitContainer.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtProgram
+			// 
+			this.txtProgram.AcceptsReturn = true;
+			this.txtProgram.AcceptsTab = true;
+			this.txtProgram.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtProgram.ImeMode = System.Windows.Forms.ImeMode.On;
+			this.txtProgram.Location = new System.Drawing.Point(5, 32);
+			this.txtProgram.Multiline = true;
+			this.txtProgram.Name = "txtProgram";
+			this.txtProgram.Size = new System.Drawing.Size(377, 533);
+			this.txtProgram.TabIndex = 0;
+			this.txtProgram.Text = "「こんにちは」を出力する。";
+			this.txtProgram.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtProgram_KeyDown);
+			// 
+			// txtOut
+			// 
+			this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtOut.BackColor = System.Drawing.Color.White;
+			this.txtOut.Location = new System.Drawing.Point(5, 32);
+			this.txtOut.Multiline = true;
+			this.txtOut.Name = "txtOut";
+			this.txtOut.ReadOnly = true;
+			this.txtOut.Size = new System.Drawing.Size(388, 533);
+			this.txtOut.TabIndex = 1;
+			this.txtOut.TabStop = false;
+			// 
+			// lblProgram
+			// 
+			this.lblProgram.AutoSize = true;
+			this.lblProgram.Location = new System.Drawing.Point(3, 8);
+			this.lblProgram.Name = "lblProgram";
+			this.lblProgram.Size = new System.Drawing.Size(50, 12);
+			this.lblProgram.TabIndex = 2;
+			this.lblProgram.Text = "プログラム";
+			// 
+			// lblOut
+			// 
+			this.lblOut.AutoSize = true;
+			this.lblOut.Location = new System.Drawing.Point(3, 8);
+			this.lblOut.Name = "lblOut";
+			this.lblOut.Size = new System.Drawing.Size(53, 12);
+			this.lblOut.TabIndex = 3;
+			this.lblOut.Text = "実行結果";
+			// 
+			// btnExecute
+			// 
+			this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExecute.Location = new System.Drawing.Point(307, 3);
+			this.btnExecute.Name = "btnExecute";
+			this.btnExecute.Size = new System.Drawing.Size(75, 23);
+			this.btnExecute.TabIndex = 3;
+			this.btnExecute.Text = "実行";
+			this.btnExecute.UseVisualStyleBackColor = true;
+			this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+			// 
+			// splitContainer
+			// 
+			this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer.Location = new System.Drawing.Point(12, 12);
+			this.splitContainer.Name = "splitContainer";
+			// 
+			// splitContainer.Panel1
+			// 
+			this.splitContainer.Panel1.Controls.Add(this.btnOpen);
+			this.splitContainer.Panel1.Controls.Add(this.btnSave);
+			this.splitContainer.Panel1.Controls.Add(this.lblProgram);
+			this.splitContainer.Panel1.Controls.Add(this.btnExecute);
+			this.splitContainer.Panel1.Controls.Add(this.txtProgram);
+			// 
+			// splitContainer.Panel2
+			// 
+			this.splitContainer.Panel2.Controls.Add(this.lblOut);
+			this.splitContainer.Panel2.Controls.Add(this.txtOut);
+			this.splitContainer.Size = new System.Drawing.Size(785, 568);
+			this.splitContainer.SplitterDistance = 385;
+			this.splitContainer.TabIndex = 5;
+			this.splitContainer.TabStop = false;
+			// 
+			// btnSave
+			// 
+			this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSave.Location = new System.Drawing.Point(226, 3);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 2;
+			this.btnSave.Text = "保存";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnOpen.Location = new System.Drawing.Point(145, 3);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(75, 23);
+			this.btnOpen.TabIndex = 1;
+			this.btnOpen.Text = "開く";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// StartForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(809, 592);
+			this.Controls.Add(this.splitContainer);
+			this.Name = "StartForm";
+			this.Text = "クロガネエディタ";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtProgram_KeyDown);
+			this.splitContainer.Panel1.ResumeLayout(false);
+			this.splitContainer.Panel1.PerformLayout();
+			this.splitContainer.Panel2.ResumeLayout(false);
+			this.splitContainer.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+			this.splitContainer.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtProgram;
+		private System.Windows.Forms.TextBox txtOut;
+		private System.Windows.Forms.Label lblProgram;
+		private System.Windows.Forms.Label lblOut;
+		private System.Windows.Forms.Button btnExecute;
+		private System.Windows.Forms.SplitContainer splitContainer;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnOpen;
+	}
+}
+
---
+++ SimpleEditor/StartForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Kurogane;
+using System.IO;
+using Kurogane.Compiler;
+
+namespace Kurogane.SimpleEditor {
+	public partial class StartForm : Form {
+
+		public StartForm() {
+			InitializeComponent();
+		}
+
+		private void Execute() {
+			var engine = new Engine();
+			var writer = new StringWriter();
+			engine.Out = writer;
+			var code = txtProgram.Text;
+			try {
+				engine.Execute(code);
+				txtOut.ForeColor = Color.Black;
+				txtOut.Text = writer.ToString();
+			}
+			catch (CompilerException ex) {
+				txtOut.ForeColor = Color.Red;
+				txtOut.Text = "コンパイルエラーです。" + Environment.NewLine +
+					ex.StackTrace;
+			}
+			catch (Exception ex){
+				txtOut.ForeColor = Color.Red;
+				txtOut.Text = ex.StackTrace;
+			}
+		}
+
+		private void OpenFile() {
+			var win = new OpenFileDialog();
+			win.FileName = "プログラム.krg";
+			win.Filter = "クロガネファイル(*.krg)|*.krg|すべてのファイル|*.*";
+			win.RestoreDirectory = true;
+			if (win.ShowDialog() == DialogResult.OK) {
+				txtProgram.Text = File.ReadAllText(win.FileName, Encoding.Default);
+			}
+		}
+
+		private void Save() {
+			var win = new SaveFileDialog();
+			win.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			win.FileName = "プログラム.krg";
+			win.Filter = "クロガネファイル(*.krg)|*.krg";
+			win.RestoreDirectory = true;
+			if (win.ShowDialog() == DialogResult.OK) {
+				File.WriteAllText(win.FileName, txtProgram.Text, Encoding.Default);
+			}
+		}
+
+		private void txtProgram_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.F5)
+				Execute();
+			if (e.KeyCode == (Keys.ControlKey | Keys.S))
+				Save();
+			if (e.KeyCode == (Keys.ControlKey | Keys.O))
+				OpenFile();
+		}
+
+		private void btnExecute_Click(object sender, EventArgs e) {
+			Execute();
+		}
+
+		private void btnSave_Click(object sender, EventArgs e) {
+			Save();
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e) {
+			OpenFile();
+		}
+	}
+}
