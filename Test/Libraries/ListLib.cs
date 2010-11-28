98945464cbd87f5eaee6de7150f23bab231a1e5f
OTA Daichi <irxnjhtchlnrw@gmail.com>
2010-11-28 06:55:19 +0000

    テスト修正中
    git-svn-id: https://kurogane.svn.codeplex.com/svn/trunk@80749 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ Test/Libraries/ListLib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kurogane.Test.Libraries {
+
+	[TestClass]
+	public class ListLib : NoSideEffectTest {
+
+		[TestMethod]
+		public void 平坦化() {
+			Assert.AreEqual(
+				ListCell.Null,
+				Execute<ListCell>("無を平坦化する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom(3),
+				Execute<ListCell>("3を平坦化する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom(3, 5, 7),
+				Execute<ListCell>("３と５と７を平坦化する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom(1, 2, 3),
+				Execute<ListCell>("[1,2,3]を平坦化する。"));
+
+			Assert.AreEqual(
+				ListCell.ConvertFrom(0, 1, 2, 3, 4, 5, 6),
+				Execute<ListCell>("[0,[],[1],[2,3],[4,[],[5,[6]]]]を平坦化する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom(1, 2, 3, 4, 5, 6, 7, 8, 9),
+				Execute<ListCell>("(((1:2):(3:4:5)):(6:(7:8):9))を平坦化する。"));
+		}
+
+	}
+}
---
+++ Test/Libraries/StringLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kurogane.Test.Libraries {
+
+	[TestClass]
+	public class StringLib : NoSideEffectTest {
+
+		[TestMethod]
+		public void 文字分割() {
+			Assert.AreEqual(ListCell.Null, Execute<ListCell>("無を文字分割する。"));
+			Assert.AreEqual(ListCell.Null, Execute<ListCell>("「」を文字分割する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom(new[] { "あ" }),
+				Execute<ListCell>("「あ」を文字分割する。"));
+			Assert.AreEqual(
+				ListCell.ConvertFrom("あ", "い", "う", "え", "お"),
+				Execute<ListCell>("「あいうえお」を文字分割する。"));
+		}
+	}
+}
